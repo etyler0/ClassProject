49ec3226d95b2af99b5a583c4cdb6b3e5bef87f7
Dale Tyler <edt@tylerent.com>
2018-05-15 21:53:30 -0700

    update scrum doc, various headers for Doxygen

--- MainWindow/parser.h
+++ MainWindow/parser.h
@@ -5,7 +5,21 @@
 
 using namespace nserkkvector;
 
+//! readfile - read shapes.txt from disk and populate a vector containing
+//!            the parsed shapes
+//!
+//! \author kole 05/15/18)
+//!
+//! \param device - QPaintDevice *
+//! \param pShapeVector - Pointer to Myvector<Shape> to fill in
 void readFile(QPaintDevice *device, MyVector<Shape *> *pShapeVector);
+
+//! writefile - iterate through vector containing shapes, convert them
+//!             to external format and write them back to shapes.txt
+//!
+//! \author kole (5/15/18)
+//!
+//! \param pShapeVector - Pointer to Myvector<Shape> to iterate 
 void writeFile(MyVector<Shape *> *pShapeVector);
 
 #endif // PARSER_H
--- MainWindow/reports.h
+++ MainWindow/reports.h
@@ -11,27 +11,91 @@ namespace Ui {
 class reports;
 }
 
+//! reports - dialog area for reports
+//!
+//! \author richard (5/15/18)
 class reports : public QWidget
 {
     Q_OBJECT
 
 public:
+    //! constructor - craete an area to render the reports
+    //!
+    //! \author richard (5/15/18)
+    //!
+    //! \param parent 
+    //! \param sort 
     explicit reports(QWidget *parent = 0, int sort = 0);
+
+    //! setSort - set internal sorter member
+    //!
+    //! \author richard (5/15/18)
+    //!
+    //! \param val - sorter value
     void setSort(int val);
-    void setVector(MyVector<Shape*> *temp);
+
+    //! setVector - set internal pointer to appwide MyVector<Shape>*
+    //!
+    //! \author richard (5/15/18)
+    //!
+    //! \param temp - MyVector<Shape>*
+    void setVector(MyVector<Shape *> *temp);
+
+    //! show - draw on canvas
+    //!
+    //! \author richard (5/15/18)
     void show();
+
+    //! shapeToString - convert shapeType enum to string describing shape
+    //!
+    //! \author richard (5/15/18)
+    //!
+    //! \param shape - shape ID
+    //!
+    //! \return QString 
     QString shapeToString(int shape);
+
+    //! Destuctor - deallocate local members
+    //!
+    //! \author edt (5/15/18)
     ~reports();
 
 protected:
     //  Mouse events
-    QPoint mousePoint;
+    QPoint mousePoint; ///< current mouse location
+
+    //! mousePressEvent - process a mouse click down event
+    //!
+    //! \author richard (5/15/18)
+    //!
+    //! \param event 
     void mousePressEvent(QMouseEvent *event);
+
+    //! mouseReleaseEvent - process a mouse click release event
+    //!
+    //! \author richard (5/15/18)
+    //!
+    //! \param event 
     void mouseReleaseEvent(QMouseEvent *event);
+
+    //! mousePressEvent - process a mouse move event
+    //!
+    //! \author edt (5/15/18)
+    //!
+    //! \param event 
     void mouseMoveEvent(QMouseEvent *event);
 
 private slots:
+    //!on_pushButton_report_exit_clicked - process exit clicked event
+    //!
+    //! \author richard (5/15/18)
     void on_pushButton_report_exit_clicked();
+
+    //! on_comboBox_report_sort_currentIndexChanged - - process event
+    //!
+    //! \author richard (5/15/18)
+    //!
+    //! \param index 
     void on_comboBox_report_sort_currentIndexChanged(int index);
 
 private:
